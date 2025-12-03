abc35c1af446d62bd8f2e7ae3f326070d02c82f4
fikkomuharavid <xxfikkoxx@gmail.com>
2025-12-03 15:21:56 +0700

    tes

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
 kontakkkk
+<i class="bi bi-house-door-fill" style="color: blue;"></i>
+<i class="bi bi-person-circle" style="color: #007bff;"></i>
 
 @endsection
